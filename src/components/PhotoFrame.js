5b0f7cd27853b75ae4a7794dc39c4ec74592b4f6
jhuamaniCond <jhuamanicond@unsa.edu.pe>
2024-12-07 19:45:56 -0500

    estilos basicos para el background

--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -4,6 +4,7 @@ import { ReactComponent as IconChange } from '../icons/change.svg';
 import { ReactComponent as IconEdit } from '../icons/edit.svg';
 import { ReactComponent as IconEffect } from '../icons/effects.svg';
 import { ReactComponent as IconSticker } from '../icons/sticker.svg';
+import { ReactComponent as IconSun } from '../icons/sun.svg';
 
 const PhotoFrame = () => {
   const [time, setTime] = useState(new Date());
@@ -64,8 +65,6 @@ const PhotoFrame = () => {
     if (adornEffect === "") {
       setAdornEffect("rain");
     } else if (adornEffect === "rain") {
-      setAdornEffect("hojas");
-    } else if (adornEffect === "hojas") {
       setAdornEffect("azules");
     } else if (adornEffect === "azules") {
       setAdornEffect("snow");
@@ -125,146 +124,147 @@ const PhotoFrame = () => {
           backgroundImage: `url(${backgroundImage})`,
           filter: `brightness(${adjustments.brightness}%) saturate(${adjustments.saturation}%) contrast(${adjustments.contrast}%)`,
         }}
-      >
-        <div className="info-container">
-          <div className="time">
-            {time.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
-          </div>
-          <div className="weather">
-            <div className="temperature">{weather.temperature}</div>
-            <div className="description">{weather.description}</div>
-          </div>
+      ></div>
+    <div className="info-container">
+        <div className="time">
+        {time.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
         </div>
-        <div className="containerButtons">
-            <IconChange className="menu-button" onClick={() => setIsModalOpen(true)}/>
-            <IconEdit className="menu-button" onClick={() => setIsEditPanelOpen(!isEditPanelOpen)}/>
-            <IconEffect className="menu-button" onClick={toggleAdorns}/>
-            <IconSticker className="menu-button" onClick={() => setIsStickerPanelOpen(true)}/>
+        <div className="weather">
+        <div className="temperature">{weather.temperature}</div>
+        <div className="description">{weather.description}</div>
+        <IconSun className="sun"></IconSun>
         </div>
-        
-        
+    </div>
+    <div className="containerButtons">
+        <IconChange className="menu-button" onClick={() => setIsModalOpen(true)}/>
+        <IconEdit className="menu-button" onClick={() => setIsEditPanelOpen(!isEditPanelOpen)}/>
+        <IconEffect className="menu-button" onClick={toggleAdorns}/>
+        <IconSticker className="menu-button" onClick={() => setIsStickerPanelOpen(true)}/>
+    </div>
+    
+    
 
-        {isEditPanelOpen && (
-          <div className="edit-panel">
-            <h4>Editar Imagen</h4>
-            <label>
-              Brillo:
-              <input
-                type="range"
-                name="brightness"
-                min="50"
-                max="200"
-                value={adjustments.brightness}
-                onChange={handleAdjustmentChange}
-              />
-            </label>
-            <label>
-              Saturación:
-              <input
-                type="range"
-                name="saturation"
-                min="50"
-                max="200"
-                value={adjustments.saturation}
-                onChange={handleAdjustmentChange}
-              />
-            </label>
-            <label>
-              Contraste:
-              <input
-                type="range"
-                name="contrast"
-                min="50"
-                max="200"
-                value={adjustments.contrast}
-                onChange={handleAdjustmentChange}
-              />
-            </label>
-          </div>
-        )}
+    {isEditPanelOpen && (
+        <div className="edit-panel">
+        <h4>Editar Imagen</h4>
+        <label>
+            Brillo:
+            <input
+            type="range"
+            name="brightness"
+            min="50"
+            max="200"
+            value={adjustments.brightness}
+            onChange={handleAdjustmentChange}
+            />
+        </label>
+        <label>
+            Saturación:
+            <input
+            type="range"
+            name="saturation"
+            min="50"
+            max="200"
+            value={adjustments.saturation}
+            onChange={handleAdjustmentChange}
+            />
+        </label>
+        <label>
+            Contraste:
+            <input
+            type="range"
+            name="contrast"
+            min="50"
+            max="200"
+            value={adjustments.contrast}
+            onChange={handleAdjustmentChange}
+            />
+        </label>
+        </div>
+    )}
+    
+    {stickers.map((sticker) => (
+        <img
+        key={sticker.id}
+        src={sticker.url}
+        alt="Sticker"
+        className="sticker"
+        style={{
+            top: `${sticker.y}px`,
+            left: `${sticker.x}px`,
+            width : '100px',
+            height : '100px'     
+        }}
+        onClick={()=>{removeSticker(sticker.id);setActiveSticker({ id:sticker.id,url: sticker.url, x: sticker.x, y: sticker.y, size: 100 })}}
+        />
+    ))}
+    {activeSticker && (
+        <div
+        className="active-sticker"
+        style={{
+            top: `${activeSticker.y}px`,
+            left: `${activeSticker.x}px`,
+            width : '100px',
+            height : '100px'    
+        }}
+        onClick={fixSticker}
+        >
+        <img src={activeSticker.url} alt="Active Sticker"
+        style={{
+            width : '100px',
+                height : '100px'    
+            }} />
         
-        {stickers.map((sticker) => (
-          <img
-            key={sticker.id}
-            src={sticker.url}
-            alt="Sticker"
-            className="sticker"
-            style={{
-              top: `${sticker.y}px`,
-              left: `${sticker.x}px`,
-               width : '100px',
-               height : '100px'     
-            }}
-            onClick={()=>{removeSticker(sticker.id);setActiveSticker({ id:sticker.id,url: sticker.url, x: sticker.x, y: sticker.y, size: 100 })}}
-          />
-        ))}
-        {activeSticker && (
-          <div
-            className="active-sticker"
-            style={{
-              top: `${activeSticker.y}px`,
-              left: `${activeSticker.x}px`,
-              width : '100px',
-               height : '100px'    
-            }}
-            onClick={fixSticker}
-          >
-            <img src={activeSticker.url} alt="Active Sticker"
-            style={{
-                width : '100px',
-                 height : '100px'    
-              }} />
-            
-          </div>
-        )}
-      </div>
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <h2>Seleccionar Imagen</h2>
-            <div className="image-gallery">
-              {images.map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`Imagen ${index + 1}`}
-                  onClick={() => handleImageSelect(image)}
-                  className="gallery-image"
-                />
-              ))}
-            </div>
-            <button
-              className="close-modal"
-              onClick={() => setIsModalOpen(false)}
-            >
-              Cerrar
-            </button>
-          </div>
         </div>
-      )}
-      {isStickerPanelOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <h2>Seleccionar Sticker</h2>
-            <div className="image-gallery">
-              {stickerOptions.map((sticker, index) => (
-                <img
-                  key={index}
-                  src={sticker}
-                  alt={`Sticker ${index + 1}`}
-                  onClick={() => handleStickerSelect(sticker)}
-                  className="gallery-image"
-                />
-              ))}
-            </div>
-            <button
-              className="close-modal"
-              onClick={() => setIsStickerPanelOpen(false)}
-            >
-              Cerrar
-            </button>
-          </div>
+    )}
+    
+    {isModalOpen && (
+    <div className="modal">
+        <div className="modal-content">
+        <h2>Seleccionar Imagen</h2>
+        <div className="image-gallery">
+            {images.map((image, index) => (
+            <img
+                key={index}
+                src={image}
+                alt={`Imagen ${index + 1}`}
+                onClick={() => handleImageSelect(image)}
+                className="gallery-image"
+            />
+            ))}
         </div>
+        <button
+            className="close-modal"
+            onClick={() => setIsModalOpen(false)}
+        >
+            Cerrar
+        </button>
+        </div>
+    </div>
+    )}
+    {isStickerPanelOpen && (
+    <div className="modal">
+        <div className="modal-content">
+        <h2>Seleccionar Sticker</h2>
+        <div className="image-gallery">
+            {stickerOptions.map((sticker, index) => (
+            <img
+                key={index}
+                src={sticker}
+                alt={`Sticker ${index + 1}`}
+                onClick={() => handleStickerSelect(sticker)}
+                className="gallery-image"
+            />
+            ))}
+        </div>
+        <button
+            className="close-modal"
+            onClick={() => setIsStickerPanelOpen(false)}
+        >
+            Cerrar
+        </button>
+        </div>
+    </div>
       )}
     </div>
   );
